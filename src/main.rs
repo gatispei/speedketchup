4a77f774c5b94fe3ed6700a011e7248a66f39c99
Gatis Peisenieks <gatis@mikrotik.com>
2023-10-20 21:19:48 +0300

    netwatch: switch reqwest->ureq, use only rustls, disable openssl release binary 6.7->1.7MB

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ impl std::fmt::Display for ErrorString {
     }
 }
 
-impl From<reqwest::Error> for ErrorString {
-    fn from(err: reqwest::Error) -> ErrorString {
+impl From<ureq::Error> for ErrorString {
+    fn from(err: ureq::Error) -> ErrorString {
         ErrorString(err.to_string())
     }
 }
-impl From<::std::io::Error> for ErrorString {
+impl From<std::io::Error> for ErrorString {
     fn from(err: std::io::Error) -> ErrorString {
         ErrorString(err.to_string())
     }
@@ -40,22 +40,15 @@ impl From<roxmltree::Error> for ErrorString {
     }
 }
 
-pub fn download_configuration() -> Result<bool, ErrorString> {
-    let client = reqwest::blocking::Client::new();
-    let config_xml = client.get("http://www.speedtest.netlskdj/speedtest-config.php")
-        .header(reqwest::header::CONNECTION, "close")
-        .header(reqwest::header::USER_AGENT, "netwatch")
-        .send()?.text()?;
-//    println!("{}", config_xml);
 
+pub fn download_configuration() -> Result<bool, ErrorString> {
+    let config_xml: String = ureq::get("http://www.speedtest.net/speedtest-config.php")
+        .call()?.into_string()?;
     let config = roxmltree::Document::parse(&config_xml)?;
     println!("config: {:?}", config);
 
-    let servers_xml = client
-        .get("http://www.speedtest.net/speedtest-servers.php")
-        .header(reqwest::header::CONNECTION, "close")
-        .header(reqwest::header::USER_AGENT, "netwatch")
-        .send()?.text()?;
+    let servers_xml: String = ureq::get("http://www.speedtest.net/speedtest-servers.php")
+        .call()?.into_string()?;
     let servers = roxmltree::Document::parse(&servers_xml)?;
     println!("servers: {:?}", servers);
     Ok(true)
@@ -80,84 +73,12 @@ fn parse_config<'cfg>(config: &'cfg str) -> Config<'cfg> {
     }
 }
  */
-static _BLA: &str = "hei";
 
-fn type_of<T>(_: &T) -> &'static str {
-    return std::any::type_name::<T>();
-}
-
-fn xxx<'a>(par: &'a mut String, par2: &String) -> &'a str {
-    *par += " add ";
-    *par += par2;
-//    par.as_str();
-    _BLA
-}
-
-enum Bla {
-    Opt1,
-    Opt2(u8),
-    Opt3([u32; 10]),
-}
-
-enum Bla5 {
-    Opt5([u8; 39]),
-    Opt6(u8),
-    Opt7(u32),
-}
-
-enum Bla2 {
-    Opt2(Bla5),
-    Opt1(Bla),
-    Opt3,
-}
+//fn type_of<T>(_: &T) -> &'static str {
+//    return std::any::type_name::<T>();
+//}
 
 fn main() {
-    //    let e = Bla::Opt2(7);
-    let e1 = Bla2::Opt1(Bla::Opt1);
-    let e2 = Bla2::Opt1(Bla::Opt2(99));
-    let e3 = Bla2::Opt1(Bla::Opt3([99; 10]));
-    let e4 = Bla2::Opt2(Bla5::Opt5([5; 39]));
-    let e5 = Bla2::Opt2(Bla5::Opt6(100));
-    let e6 = Bla2::Opt2(Bla5::Opt7(100));
-    let e7 = Bla2::Opt3;
-    unsafe {
-	println!("e1: {:?}", std::mem::transmute::<Bla2, [u8; 44]>(e1));
-	println!("e2: {:?}", std::mem::transmute::<Bla2, [u8; 44]>(e2));
-	println!("e3: {:?}", std::mem::transmute::<Bla2, [u8; 44]>(e3));
-	println!("e4: {:?}", std::mem::transmute::<Bla2, [u8; 44]>(e4));
-	println!("e5: {:?}", std::mem::transmute::<Bla2, [u8; 44]>(e5));
-	println!("e6: {:?}", std::mem::transmute::<Bla2, [u8; 44]>(e6));
-	println!("e7: {:?}", std::mem::transmute::<Bla2, [u8; 44]>(e7));
-    }
-    let mut s = String::from("bla");
-    let s2 = String::from("bla2");
-//    {
-    let ss: &str = xxx(& mut s, & s2);
-    println!("ss: {}", ss);
-    println!("s: {}", s);
-//    }
-    println!("s2: {}", s2);
-
-    let s = "bla";
-    println!("s: {:?}", s);
-    println!("typeof s: {}", type_of(&s));
-    let a = Some(5);
-    println!("a: {:?}", a);
-    println!("typeof a: {}", type_of(&a));
-    let x: u8 = 5;
-    let y: u16 = x as u16 + 255;
-    println!("x: {}", x);
-
-    let z: u8 = y as u8;
-    println!("z: {}", z);
-
-    let zzz: Result<u8, _> = y.try_into();
-    println!("typeof zzz: {}", type_of(&zzz));
-    println!("zzz:{:?}", zzz);
-    println!("zzz.err: {}", zzz.err().unwrap().to_string());
-    let res: Result<u32, _> = "bla".parse();
-    println!("typeof res: {}", type_of(&res));
-//    println!("sizeof ErrorString: {}", std::mem::size_of::<ErrorString>());
 /*
     backtrace::trace(|frame| {
 	println!("backtrace: {:?}", frame);
@@ -167,61 +88,11 @@ fn main() {
 	true
     });
 */
-    println!("sizeof Bla: {}", std::mem::size_of::<Bla>());
-    println!("sizeof Bla2: {}", std::mem::size_of::<Bla2>());
     println!("sizeof opt: {}", std::mem::size_of::<Option<u8>>());
     println!("sizeof opt: {}", std::mem::size_of::<Option<u16>>());
     println!("sizeof opt: {}", std::mem::size_of::<Option<u32>>());
     println!("sizeof opt: {}", std::mem::size_of::<Option<u64>>());
     println!("sizeof opt: {}", std::mem::size_of::<Option<u128>>());
-    println!("sizeof reqwest::Error: {}", std::mem::size_of::<reqwest::Error>());
-    println!("sizeof std::io::Error: {}", std::mem::size_of::<std::io::Error>());
-    println!("sizeof std::num::ParseFloatError: {}", std::mem::size_of::<std::num::ParseFloatError>());
-    println!("sizeof std::num::ParseIntError: {}", std::mem::size_of::<std::num::ParseIntError>());
-    println!("sizeof std::net::AddrParseError: {}", std::mem::size_of::<std::net::AddrParseError>());
-    println!("sizeof roxmltree::Error: {}", std::mem::size_of::<roxmltree::Error>());
-
-    println!("sizeof std::num::ParseIntError: {}", std::mem::size_of::<std::num::ParseIntError>());
-    println!("sizeof &std::num::ParseIntError: {}", std::mem::size_of::<&std::num::ParseIntError>());
-    println!("sizeof u8: {}", std::mem::size_of::<u8>());
-    println!("sizeof u16: {}", std::mem::size_of::<u16>());
-    println!("sizeof u32: {}", std::mem::size_of::<u32>());
-    println!("sizeof u64: {}", std::mem::size_of::<u64>());
-    println!("sizeof u128: {}", std::mem::size_of::<u128>());
-
-    println!("sizeof &u8: {}", std::mem::size_of::<&u8>());
-    println!("sizeof &u16: {}", std::mem::size_of::<&u16>());
-    println!("sizeof &u32: {}", std::mem::size_of::<&u32>());
-    println!("sizeof &u64: {}", std::mem::size_of::<&u64>());
-    println!("sizeof &u128: {}", std::mem::size_of::<&u128>());
-
-    println!("sizeof res8: {}", std::mem::size_of::<Result<u8, std::num::ParseIntError>>());
-    println!("sizeof res16: {}", std::mem::size_of::<Result<u16, std::num::ParseIntError>>());
-    println!("sizeof res32: {}", std::mem::size_of::<Result<u32, std::num::ParseIntError>>());
-    println!("sizeof res64: {}", std::mem::size_of::<Result<u64, std::num::ParseIntError>>());
-    println!("sizeof res128: {}", std::mem::size_of::<Result<u128, std::num::ParseIntError>>());
-
-    println!("sizeof res8: {}", std::mem::size_of::<Result<&u8, &std::num::ParseIntError>>());
-    println!("sizeof res16: {}", std::mem::size_of::<Result<&u16, &std::num::ParseIntError>>());
-    println!("sizeof res32: {}", std::mem::size_of::<Result<&u32, &std::num::ParseIntError>>());
-    println!("sizeof res64: {}", std::mem::size_of::<Result<&u64, &std::num::ParseIntError>>());
-    println!("sizeof res128: {}", std::mem::size_of::<Result<&u128, &std::num::ParseIntError>>());
-    //    println!("typeof res: {}", typeof::type_of(res));
-    //    println!("res.ok: {:?}", res.ok());
-    let res2 = res.as_ref();
-    println!("typeof res2: {}", type_of(&res2));
-    if res2.is_err() {
-	println!("res.err: {}", res2.err().unwrap().to_string());
-    }
-    if res2.is_err() {
-	println!("res.err: {:?}", res2.err());
-    }
-    if res2.is_err() {
-	println!("res.err: {:#?}", res2.err());
-    }
-    println!("res.err: {:?}", res.err().unwrap().to_string());
-//    return;
-//    let _guess: u32 = "bla".parse().expect("Not a num!");
 
     std::env::set_var("RUST_BACKTRACE", "1");
 //    println!("Hello World!");
